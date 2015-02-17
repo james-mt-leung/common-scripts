6adc93b6a1a87dc3ee110a6433508bd5ecf6a3f4
James Man Tik Leung <jleung@xmatters.com>
2015-02-17 10:50:29 -0800

    Add python packages (missing packages)

---
+++ home/.emacs.d/elpa/popup-20150116.1223/popup-autoloads.el
@@ -0,0 +1,18 @@
+;;; popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("popup-pkg.el" "popup.el") (21731 35167
+;;;;;;  356393 0))
+
+;;;***
+
+(provide 'popup-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; popup-autoloads.el ends here
